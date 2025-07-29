b6c8ca9b00158111101724b1b5c3876eace25268
Jedsadaporn05 <jedsadapornnew05@gmail.com>
2025-07-29 15:24:35 +0700

    feat: connect REST Api with MySQL database

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,35 @@
-// เรียกใช้ library express ด้วยคำสั่ง require
+// กำหนด port ที่ 8000
+const port = 8000;
+// เรียกใช้ library express ด้วยคำสั่ง require หรือหากเป็น TypeScript ให้ใช้การ import
 import express, { Request, Response } from "express";
 // ประกาศเริ่มต้นการใช้ express
 const app = express();
-// กำหนด port ที่ 8000
-
-const port = 8000;
-// เรียกใช้ bodyParser ด้วยคำสั่ง require
+// เรียกใช้ library mysql2
+import mysql from "mysql2/promise";
+// เรียกใช้ library cors (Corss-Origin Resource Sharing)
+import cors from "cors";
+// เรียกใช้ bodyParser ด้วยคำสั่ง require หรือหากเป็น TypeScript ให้ใช้การ import
 import bodyParser from "body-parser";
 
 // ให้ express ใช้ bodyParser
 app.use(bodyParser.json());
+// ให้ express ใช้ cors
+app.use(cors());
 
+// สร้างตัวแปรสำหรับเชื่อต่อ MySQL
+let conn: any = null;
+// ฟังก์ชันเชื่อมต่อ MySQL
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "tutorials",
+    port: 8889, // ปกติจะเป็น 3306 แต่เราใช้ 8889
+  });
+};
+
+// สร้างฟังก์ชัน Validate ข้อมูล
 const validateData = (newTask: any) => {
   let errors = [];
 
@@ -23,7 +42,7 @@ const validateData = (newTask: any) => {
   }
   return errors;
 };
-
+// สร้างฟังก์ชัน Validate สำหรับการ PUT หรืออัพเดทข้อมูลเข้ามาใหม่
 const validateUpdate = (newTask: any) => {
   let error = [];
 
@@ -31,34 +50,26 @@ const validateUpdate = (newTask: any) => {
     error.push("isCompleted must be a boolean");
   }
   return error;
-}
-
-// กำหนด model data
-interface Task {
-  id: Number;
-  title: String;
-  isCompleted: Boolean;
-}
-
-// สร้างตัวแปร users ขึ้นมาเป็น Array จำลองการเก็บข้อมูลใน Server (ซึ่งของจริงจะเป็น database)
-let tasks: Task[] = [];
+};
 
 // GET ดึงข้อมูลออกมาทั้งหมด
-app.get("/tasks", (req: Request, res: Response) => {
-  res.json(tasks);
+app.get("/tasks", async (req: Request, res: Response) => {
+  const results = await conn.query("SELECT * FROM tasks");
+  // Server response ตอบกลับด้วยข้อมูลใน results ที่ดึงข้อมูลมาจาก MySQL เป็นแบบ json
+  res.json(results[0]);
 });
 
 // GET ดึงข้อมูลออกมารายตัว GET by ID
-app.get("/tasks/:id", (req: Request, res: Response) => {
+app.get("/tasks/:id", async (req: Request, res: Response) => {
   try {
     let id = Number(req.params.id);
-    const task = tasks.find((task) => task.id == id);
+    const results = await conn.query("SELECT * FROM tasks WHERE id = ?", id);
 
-    if (!task) {
+    if (!results[0] || results[0].length == 0) {
       throw { statusCode: 404, message: "User not found" };
     }
-
-    res.json(task);
+    // Server response ตอบกลับมาด้วยข้อมูลใน results ที่เป็นการดึงข้อมูลมาจาก id ใน MySQL เป็นแบบ json
+    res.json(results[0][0]);
   } catch (error: any) {
     let statusCode = error.statusCode || 500;
     res.status(statusCode).json({
@@ -68,10 +79,10 @@ app.get("/tasks/:id", (req: Request, res: Response) => {
 });
 
 // POST สร้าง Task ใหม่ขึ้นมา
-app.post("/tasks", (req: Request, res: Response) => {
+app.post("/tasks", async (req: Request, res: Response) => {
   try {
     const data = req.body;
-
+    // Validate ข้อมูลก่อนเพิ่มเข้า
     const errors = validateData(data);
     if (errors.length > 0) {
       throw {
@@ -79,20 +90,13 @@ app.post("/tasks", (req: Request, res: Response) => {
         errors: errors,
       };
     }
-
-    const newTask = {
-      id: tasks.length + 1,
-      title: data.title,
-      isCompleted: data.isCompleted || false,
-    };
-
-    // push newUser เข้าไปใน array ของ users
-    tasks.push(newTask);
-
-    // Server ตอบกลับมาว่าเพิ่มเรียบร้อยแล้ว
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: newTask });
+    const results = await conn.query("INSERT INTO tasks SET ?", data);
+    // Server response ตอบกลับมาว่าเพิ่มเรียบร้อยแล้ว ด้วย statusCode 201
+    res.status(201).json({
+      message: "User created successfully",
+      id: results[0].insertId,
+      data: data,
+    });
   } catch (error: any) {
     console.error("error message", error);
     res.status(501).json({
@@ -103,12 +107,16 @@ app.post("/tasks", (req: Request, res: Response) => {
 });
 
 // PUT แก้ไขข้อมูล Task
-app.put("/tasks/:id", (req: Request, res: Response) => {
+app.put("/tasks/:id", async (req: Request, res: Response) => {
   try {
     let id = Number(req.params.id);
     let updateUser = req.body;
-    const task = tasks.find((task) => task.id == id);
+    const results = await conn.query("UPDATE tasks SET ? WHERE id = ?", [
+      updateUser,
+      id,
+    ]);
 
+    // Validate ข้อมูลต้องกรอกให้ถูกต้อง
     const errors = validateUpdate(updateUser);
     if (errors.length > 0) {
       throw {
@@ -116,27 +124,22 @@ app.put("/tasks/:id", (req: Request, res: Response) => {
         errors: errors,
       };
     }
-
-    if (!task) {
+    // ไม่ให้อัพเดท user id ที่ไม่มีอยู่จริง
+    if (results[0].affectedRows == 0) {
       throw { statusCode: 404, message: "User not found" };
     }
-
+    // ไม่ให้อัพเดท id
     if (updateUser.id) {
       throw {
         statusCode: 400,
         message: "Cannot update user id",
       };
     }
-
-    if (updateUser.title) {
-      task.title = updateUser.title;
-    }
-
-    if (updateUser.isCompleted) {
-      task.isCompleted = updateUser.isCompleted;
-    }
-
-    res.json(task);
+    // Server response ตอบกลับมาว่าอัพเดทเรียบร้อยแล้วแบบ json
+    res.json({
+      message: "User Updated!",
+      updateUser: updateUser,
+    });
   } catch (error: any) {
     let statusCode = error.statusCode || 500;
     res.status(statusCode).json({
@@ -147,19 +150,18 @@ app.put("/tasks/:id", (req: Request, res: Response) => {
 });
 
 // DEL ลบข้อมูล Task ด้วย ID
-app.delete("/tasks/:id", (req: Request, res: Response) => {
+app.delete("/tasks/:id", async (req: Request, res: Response) => {
   try {
     let id = Number(req.params.id);
-    const task = tasks.find((task) => task.id == id);
+    const results = await conn.query("DELETE from tasks WHERE id = ?", id);
 
-    if (!task) {
+    if (results[0].affectedRows == 0) {
       throw { statusCode: 404, message: "User not found" };
-    } else {
-      tasks = tasks.filter((task) => task.id != id);
     }
 
-    res.status(204).json({
-      message: "Delete Ok",
+    // Server response ตอบกลับมาว่าลบเรียบร้อยแล้วแบบ json
+    res.json({
+      message: "User deleted successfully",
     });
   } catch (error: any) {
     let statusCode = error.statusCode || 500;
@@ -170,6 +172,7 @@ app.delete("/tasks/:id", (req: Request, res: Response) => {
 });
 
 // ประกาศเปิด http server ที่ port 8000
-app.listen(port, () => {
+app.listen(port, async () => {
+  await initMySQL();
   console.log(`Server is running on port ${port}`);
 });
